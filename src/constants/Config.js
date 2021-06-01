ce50d4aee355f634655d46f6c2f7431ac54cf1e0
abdesslamlimam <abdesslamlimam@gmail.com>
2021-06-01 09:32:03 +0100

    last update

--- src/constants/Config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    showStatusBar: true,
-    Login: {
-      RequiredLogin: false, // required before using the app
-      AnonymousCheckout: false, // required before checkout or checkout anonymous
-    },
-
-    menu: {},
-  };
